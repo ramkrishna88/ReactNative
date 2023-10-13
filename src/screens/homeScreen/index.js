82f4666ccb6656c52b134443dffa2349e5cdb8ac
ramkrishna88 <chowdaryrk077@gmail.com>
2023-10-13 14:32:16 +0100

    Google Maps Api

--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 import styles from './styles';
 
 const HomeScreen = () => {
+  const [region, setRegion] = useState({
+    latitude: 51.509865,
+    longitude: -0.118092,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const [locationName, setLocationName] = useState('United Kingdom');
+
+  const handleMapPress = event => {
+    // Get the coordinates of the tapped location
+    const {latitude, longitude} = event.nativeEvent.coordinate;
+
+    // Update the region and location description
+    setRegion({
+      ...region,
+      latitude,
+      longitude,
+    });
+
+    // Fetch location details (reverse geocoding)
+    fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&addressdetails=1`,
+    )
+      .then(response => response.json())
+      .then(data => {
+        const location = data.display_name || 'Unknown Location';
+        setLocationName(location);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.centeredText}>This is the home screen</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={region}
+        onPress={handleMapPress}>
+        <Marker
+          coordinate={{latitude: region.latitude, longitude: region.longitude}}
+          title={locationName}
+        />
+      </MapView>
+      <View style={styles.locationDetails}>
+        <Text>{locationName}</Text>
+      </View>
     </View>
   );
 };
--- src/screens/homeScreen/styles.js
+++ src/screens/homeScreen/styles.js
@@ -3,12 +3,34 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  centeredText: {
-    textAlign: 'center',
-    fontSize: 20,
+  map: {
+    flex: 1,
+    width: '100%',
+  },
+  locationDetails: {
+    position: 'absolute',
+    bottom: 24,
+    left: 16,
+    right: 16,
+    backgroundColor: 'white',
+    padding: 8,
+    borderRadius: 8,
+  },
+  buttonContainer: {
+    flexDirection: 'coloumn',
+    marginTop: 16,
+  },
+  zoomButton: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    padding: 10,
+    margin: 8,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
   },
 });
 
