87bf99df8943d1116c5e0dd1dee007227570c156
ramkrishna88 <chowdaryrk077@gmail.com>
2023-10-13 12:58:28 +0100

    updated

--- src/navigation/NavigationScreen.js
+++ src/navigation/NavigationScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen, LoginScreen} from '@screens/index';
+import {HomeScreen, LoginScreen} from '@screens';
 
 const Stack = createNativeStackNavigator();
 
