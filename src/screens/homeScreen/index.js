b092be96009e05914f6b09a92d0abcf7f93d7531
ramkrishna88 <chowdaryrk077@gmail.com>
2023-10-23 15:57:13 +0100

    Android Image Picker

--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  PermissionsAndroid,
+} from 'react-native';
 import styles from './styles';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const HomeScreen = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedGallery, setSelectedGallery] = useState(null);
+
+  let options = {
+    saveToPhotos: true,
+    mediaType: 'photo',
+  };
+
+  const openCamera = async () => {
+    const cameraPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+    );
+
+    if (cameraPermission === PermissionsAndroid.RESULTS.GRANTED) {
+      launchCamera(options, response => {
+        if (!response.didCancel && !response.error) {
+          setSelectedImage(response.assets[0].uri);
+        }
+      });
+    }
+  };
+
+  const openGallery = async () => {
+    const result = await launchImageLibrary(options);
+    setSelectedGallery(result.assets[0].uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.centeredText}>This is the home screen</Text>
+      <TouchableOpacity style={styles.button} onPress={openCamera}>
+        <Text style={styles.text}>Launch Camera</Text>
+      </TouchableOpacity>
+
+      {selectedImage && (
+        <Image style={styles.imageStyle} source={{uri: selectedImage}} />
+      )}
+
+      <TouchableOpacity style={styles.button} onPress={openGallery}>
+        <Text style={styles.text}>Launch Gallery</Text>
+      </TouchableOpacity>
+
+      {selectedGallery && (
+        <Image style={styles.imageStyle} source={{uri: selectedGallery}} />
+      )}
     </View>
   );
 };
--- src/screens/homeScreen/styles.js
+++ src/screens/homeScreen/styles.js
@@ -3,12 +3,28 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   centeredText: {
     textAlign: 'center',
     fontSize: 20,
+  },
+  button: {
+    height: 50,
+    width: 140,
+    backgroundColor: 'skyblue',
+    padding: 16,
+    borderRadius: 10,
+    margin: 10,
+  },
+  imageStyle: {
+    width: 200,
+    height: 200,
+    borderColor: 'skyblue',
+    borderWidth: 6,
+    borderRadius: 10,
+    margin: 10,
   },
 });
 
